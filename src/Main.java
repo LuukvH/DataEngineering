c94556b5a6f247d4cc39d63439b760d13a1af192
LuukvH <laj@home.nl>
2016-03-24 15:32:23 +0100

    Added question 5
    Added question 5
    Changed output folder in all questions, to use relative paths

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by laj on 8-3-2016.
  */
@@ -23,38 +26,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-
     public static void main(String[] args) throws Exception {
-
-        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-
-        // Find smallest timespan
-        DataSet<String> text = env.readTextFile("./res/out.edit-eswiki");
-
-        DataSet<Tuple2<Integer, Long>> smallestTimespan = text
-                .flatMap(new LineSplitter())
-                .min(1);
-
-//        smallestTimespan.writeAsCsv("file:///home/jeroen/Desktop/file.csv", "\n", "|");
-//        smallestTimespan.collect();
-
         Vraag3.Solve();
     }
-
-    public static class LineSplitter implements FlatMapFunction<String, Tuple2<Integer, Long>> {
-        @Override
-        public void flatMap(String line, Collector<Tuple2<Integer, Long>> out) {
-            String[] linearguments = line.split(" ");
-
-            try {
-                int UserId = Integer.parseInt(linearguments[0]);
-                int PageId = Integer.parseInt(linearguments[1]);
-                long timestamp = Long.parseLong(linearguments[3]);
-
-                out.collect(new Tuple2<Integer, Long>(PageId, timestamp));
-            } catch(Exception ex)
-            {}
-        }
-    }
-
 }
