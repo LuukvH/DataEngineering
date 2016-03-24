bbd65a636ae80007c766df8ecef0850a43afa071
Yatoom <jmapvhoof@gmail.com>
2016-03-24 15:30:49 +0100

    Made punch card generator.

--- python/read.py
+++ python/read.py
@@ -1,11 +1,5 @@
-
-m __future__ import division
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import NullFormatter
-import scipy
-import collections
-
+from __future__ import division
+import punch
 
 def classify(edits, users):
     return classify_pop(edits) + classify_ratio(edits, users)
@@ -14,47 +8,65 @@ def classify(edits, users):
 def classify_ratio(edits, users):
     ratio = users / edits
     if ratio > 0.90:  # few edits per user (many users, few edits)
-        return "A"
+        return "F"
     if ratio > 0.8:
-        return "B"
-    if ratio > 0.7:
-        return "C"
-    if ratio > 0.6:
-        return "D"
-    if ratio > 0.5:
         return "E"
-    return "F"      # many edits per user (many edits, few users)
+    if ratio > 0.7:
+        return "D"
+    if ratio > 0.6:
+        return "C"
+    if ratio > 0.5:
+        return "B"
+    return "A"  # many edits per user (many edits, few users)
 
 
 def classify_pop(edits):
-    if edits > 1000:
-        return "A"
+    if edits >= 1000:
+        return "G"
+    if edits > 500:
+        return "F"
     if edits > 100:
-        return "B"
-    if edits > 10:
+        return "E"
+    if edits > 50:
+        return "D"
+    if edits > 20:
         return "C"
-    return "D"
+    if edits > 10:
+        return "B"
+    return "A"
 
 
 def read_row(row):
     return [int(n) for n in row.split(',')]
 
 
-chart = {}
-x = []
-y = []
+def categorize_data():
+    chart = {}
 
-with open('res/data', 'r') as data:
-    for row in data:
-        row = read_row(row)
-        edits = row[1]
-        users = row[2]
-        x.append(edits)
-        y.append(users/edits)
+    with open('../processed_data/data', 'r') as data:
+        for row in data:
+            row = read_row(row)
+            edits = row[1]
+            users = row[2]
 
-        data_class = classify(edits, users)
-        if data_class not in chart:
-            chart[data_class] = 0
-        chart[data_class] += 1
-od = collections.OrderedDict(sorted(chart.items()))
-print od
+            data_class = classify(edits, users)
+            if data_class not in chart:
+                chart[data_class] = 0
+            chart[data_class] += 1
+    return chart
+
+
+def to_key(i):
+    return ord(i[0]) - 65, ord(i[1]) - 65
+
+def create_punch_data(chart):
+    key_values = {}
+    for i, j in chart.iteritems():
+        # print to_key(i), j
+        key_values[to_key(i)] = j
+
+    return key_values
+
+chart = categorize_data()
+key_values = create_punch_data(chart)
+punch.draw_punchcard(key_values)
